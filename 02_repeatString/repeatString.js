4b55d02c0156904aea0e03d3d0cad38a2f61cbd5
Minh <ngohoangnhatminh@gmail.com>
2024-12-10 18:35:07 +0800

    Test helloWorld and repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if (n === 0) {
+        return "";
+    } else if (n < 0) {
+        return "ERROR";
+    }
 
+    let ans = str;
+    for (let i = 0; i < n - 1; i++) {
+        ans += str;
+    }
+    return ans;
 };
 
 // Do not edit below this line
