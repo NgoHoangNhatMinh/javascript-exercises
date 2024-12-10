43da21560ab74b60cd7cf39e077f4f6e6a0734b7
Minh <ngohoangnhatminh@gmail.com>
2024-12-10 20:40:42 +0800

    Submit asnwer for question 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let ans = "";
+    let n = str.length;
+    for (let i = 0; i < n; i++) {
+        ans += str[n - i - 1];
+    }
+    return ans;
 };
 
 // Do not edit below this line
